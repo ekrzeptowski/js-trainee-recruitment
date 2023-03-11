49a542af890cf81d52e233f60f647e49b2832990
Ewelina Krzeptowska <kontakt@ekrzeptowski.pl>
2023-03-11 18:43:30 +0100

    Add animal data presentation

---
+++ src/components/Animal.tsx
@@ -0,0 +1,28 @@
+import { animalType } from "@/types/animalType";
+import { useInView } from "react-intersection-observer";
+
+import styles from "./Animal.module.css";
+
+export function Animal({
+  animal,
+  setActiveAnimal,
+}: {
+  animal: animalType;
+  setActiveAnimal: (animalName: animalType["name"]) => void;
+}) {
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+    onChange: (inView) => {
+      if (inView) {
+        setActiveAnimal(animal.name);
+      }
+    },
+  });
+  return (
+    <div ref={ref} id={animal.name} className={styles.animal}>
+      <h1>{animal.name}</h1>
+      <h2 dangerouslySetInnerHTML={{ __html: animal.text }}></h2>
+      <img src={animal.image} alt={animal.name} />
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,30 @@ import { LogoNerdbord } from "../components/LogoNerdbord";
 
 import styles from "../styles/index.module.css";
 
+import { animals } from "@/data/animals";
+import { animalType } from "@/types/animalType";
+import { Animal } from "@/components/Animal";
+import { useState } from "react";
+
 export default function Home() {
+  const [activeAnimal, setActiveAnimal] = useState<animalType["name"]>(
+    animals[0].name
+  );
   return (
-    <div className={styles.wrapper}>
+    <div className={`${styles.wrapper} ${inter.className}`}>
       <LogoNerdbord />
       <h1 className={inter.className}>
         Recruitment task for Javascript Trainee
       </h1>
+      <div className={styles.animals}>
+        {animals.map((animal) => (
+          <Animal
+            key={animal.name}
+            animal={animal}
+            setActiveAnimal={setActiveAnimal}
+          />
+        ))}
+      </div>
     </div>
   );
 }
